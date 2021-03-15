007d46ab6dcb6c3fc428173dfaa5bf397610e8fa
Prabhjyotkaur16 <80683646+Prabhjyotkaur16@users.noreply.github.com>
2021-03-15 17:00:52 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,89 @@
+ <!DOCTYPE html>
+<html>
+<head>
+ <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+<style>
+body
+{
+	background-color:lightyellow;    	
+}
+h1
+{
+	background-color:grey;
+	text-align:center;
+}
+*
+{
+    padding:5;
+	margin:0;
+	
+}
+</style>
+<title>About us</title>
+</head>
+<body>
+
+	<h1 >Welcome to our site</h1>
+	<section class="my-5">
+    <div padding="py-5" >
+	<h3 class="text-center">About Us</h3>
+    </div>
+	<p >
+	<font size="5">
+	
+	Welcome to Vintage Machine where our speciality is delicious cuisine with the 
+	ingeunity and passion inspired by the fanatstic flavors of  food itself.
+	We  take great pride in improving upon your favorite food  in new and creative 
+	ways. Come and experience our cooking way!.Sitauted at NSP, rohini ,New delhi .
+	<br>
+	Drinks & snacks plus burgers, pizza & other bites in a vinatge machine with an 
+	industrial vibe.<br>
+      *  Dine-in  * Drive-through  *  No-contact delivery
+	</font>
+	<br><br>
+    <a href="home.php" class="btn btn-success" ><font size="4">Back to Main Page</font></a>  
+ 
+ </section>
+ 
+<section class="my-5">
+<div padding="py-5" >
+<h3 class="text-center">DRINKS</h3>
+</div>
+<div class="container-fluid">
+<div class="row">
+    <div class="col-lg-4 col-md-4 col-12">
+	<div class="card" >
+  <img class="card-img-top" src="images/cock1.jpg" alt="Card image" height="250">
+  
+</div>
+    </div>
+    <div class="col-lg-4 col-md-4 col-12">
+	<div class="card" >
+  <img class="card-img-top" src="images/cock2.jpg" alt="Card image">
+  
+</div>
+    </div>
+	<div class="col-lg-4 col-md-4 col-12">
+	<div class="card" >
+  <img class="card-img-top" src="images/cock3.jpg" alt="Card image">
+  
+</div>
+    </div>
+	
+</div>
+</div>
+</section>
+ <div >
+ 
+ <font size="5" >
+ <?php  echo 
+    "Our Email:catchsupport20@gmail.com<br>
+	Phone No:9458956886"?>
+	</font>
+ </div>
+ 
+
+</body>
+</html>
---
+++ home.php
@@ -0,0 +1,191 @@
+<!doctype html>
+<html>
+<head>
+ <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+<link rel="stylesheet"  href="style.css">
+
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">VINTAGE MACHINE</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="nav.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">  
+        <a class="nav-link" href="regiser.php">Register</a>
+      </li>
+	  <li class="nav-item">  
+        <a class="nav-link" href="about.php">About</a>
+      </li>
+	  <li class="nav-item">  
+        <a class="nav-link" href="contact.php">Contact</a>
+      </li>
+      
+      
+    </ul>
+    
+  </div>
+</nav>
+
+<div id="demo" class="carousel slide" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+	<li data-target="#demo" data-slide-to="3"></li>
+  </ul>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/breakfast4.jpg" alt="Break Fast" height="500">
+      <div class="carousel-caption">
+        <h3>Break Fast</h3>
+        <p>We had such a great time !</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/stake.jpg" alt="Chicago"  height="500">
+      <div class="carousel-caption">
+        <h3>Lunch</h3>
+        <p>Thank you for the lunch!</p>
+      </div>   
+    </div>
+	<div class="carousel-item">
+      <img src="images/dinner.jpg" alt="Chicago"  height="500">
+      <div class="carousel-caption">
+        <h3>Dinner</h3>
+        <p>Delicious</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/desert3.jpg" alt="New York"  height="500">
+      <div class="carousel-caption">
+        <h3>Desert</h3>
+        <p>We love the Big Apple!</p>
+      </div>   
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+
+
+<section class="my-5">
+<div padding="py-5" >
+<h3 class="text-center">ABOUT US</h3>
+</div>
+<div class="container-fluid">
+<div class="row">
+    <div class="col-lg-6  col-md-6 col-12">
+	<img src="images/din.jpg" class="image-fluid">
+    </div>
+	
+	<div class="col-lg-6  col-md-6 col-12">
+    <h3><font color="yellow">Welcome to our site</font></h3> 
+	<p class="py-3">Welcome to VINTAGE MACHINE where our speciality is delicious cuisine with the 
+	ingeunity and passion inspired by the fanatstic flavors of  food itself.
+	We  take great pride in improving upon your favorite   dishes in new and creative 
+	ways. Come and experience our cooking way!</p>
+    <a href="about.php" class="btn btn-success">KNOW MORE</a>  
+   </div>
+</div>
+
+</div>
+</section>
+
+<section class="my-5">
+<div padding="py-5" >
+<h3 class="text-center">Our Services</h3>
+</div>
+<div class="container-fluid">
+<div class="row">
+    <div class="col-lg-4 col-md-4 col-12">
+	<div class="card" >
+  <img class="card-img-top" src="images/menu.jpg" alt="Card image" height="250">
+  <div class="card-body">
+    <h4 class="card-title">Menu</a></h4>
+    
+  
+  </div>
+</div>
+    </div>
+    <div class="col-lg-4 col-md-4 col-12">
+	<div class="card" >
+  <img class="card-img-top" src="images/cock.jpg" alt="Card image">
+  <div class="card-body">
+    <h4 class="card-title">Cocktail</h4>
+    <p class="card-text">come here and have some wonderfull cocktails</p>
+
+  </div>
+</div>
+    </div>
+	 <div class="col-lg-4 col-md-4 col-12">
+	<div class="card" >
+  <img class="card-img-top" src="images/out.jpg" alt="Card image" height="250">
+  <div class="card-body">
+    <h4 class="card-title">Dinning</h4>
+    <p class="card-text">have some great food with great sitting</p>
+    
+  </div>
+</div>
+    </div>
+</div>
+</div>
+</section>
+
+<section class="my-5">
+<div padding="py-5" >
+<h3 class="text-center">Our Gallery</h3>
+</div>
+<div class="contaimer-fluid">
+<div class="row">
+    <div class="col-lg-4 col-md-4 col-12">
+	<img src="images/breakfast.jpg" class="img-fluid pb-4">
+	</div>
+	<div class="col-lg-4 col-md-4 col-12">
+	<img src="images/breakfast1.jpg" class="img-fluid pb-4">
+	</div>
+	<div class="col-lg-4 col-md-4 col-12">
+	<img src="images/breakfast2.jpg" class="img-fluid pb-4">
+	</div>
+	<div class="col-lg-4 col-md-4 col-12">
+	<img src="images/lunch.jpg" class="img-fluid pb-4">
+	</div>
+	<div class="col-lg-4 col-md-4 col-12">
+	<img src="images/lunch1.jpg" class="img-fluid pb-4">
+	</div>
+	<div class="col-lg-4 col-md-4 col-12">
+	<img src="images/lunch2.jpg" class="img-fluid pb-4">
+	</div>
+	<div class="col-lg-4 col-md-4 col-12">
+	<img src="images/desert.jpg" class="img-fluid pb-4">
+	</div>
+	<div class="col-lg-4 col-md-4 col-12">
+	<img src="images/desert1.jpg" class="img-fluid pb-4">
+	</div>
+	<div class="col-lg-4 col-md-4 col-12">
+	<img src="images/desert2.jpg" class="img-fluid pb-4">
+	</div>
+</div>
+</div>
+</section>
+
+<footer>
+<p class="text-center p-3 bg-dark text-white">@VINTAGEMACHINE </p>
+</footer>
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+</body>
+</html>
